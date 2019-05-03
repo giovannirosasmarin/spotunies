710f71d107aab5d22b99e08d80b4ae6f3cd9de5a
Giovanni Rosas-Marin <geo6240@gmail.com>
2019-05-02 20:02:56 -0700

    a

---
+++ spotunies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spotunies
+//
+//  Created by Giovanni Rosas-Marin on 5/2/19.
+//  Copyright © 2019 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
